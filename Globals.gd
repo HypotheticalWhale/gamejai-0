ccf30fee7c29f8ae10d8874fe2481f144820be85
hypotheticalwhale <samueltan.9803@gmail.com>
2025-05-24 17:13:11 +0800

    working main

--- Globals.gd
+++ Globals.gd
@@ -31,29 +31,3 @@ var tile_length = 128
 var num_rows = 3
 var picture_global_position = Vector2(480, 240)
 # end unscramble picture variables
-
-# start dialog variables
-var dialog_data = {
-	"BEGINNING": "I need to move forward.",
-	"UPON REACHING BEDROOM": "There’s nowhere to go from here.", 
-	"AFTER 30 SECONDS OF BEING IN BEDROOM": "There has to be something I can do…",
-	"AFTER OPENING CLOSET":" A secret door… I wonder how long this has been here. ",
-	"CORRIDOR DIALOGUE (NECESSARY, TO SET SCENE FOR TIER 2)":" Is it really alright to keep moving forward? I don’t know if I have the right. ",
-	"CORRIDOR DIALOGUE (NECESSARY, TO SET SCENE FOR TIER 3)":" There has to be an end to all this. There has to be something I can do to move on.",
-	"CORRIDOR DIALOGUE (UPON ENTERING CORRIDOR FOR SECOND TIME, PICTURE INCOMPLETE– HINT!)":" I can’t seem to remember her face… I wish I could see a picture. ",
-	"CORRIDOR DIALOGUE (UPON ENTERING CORRIDOR FOR SECOND TIME, CALL INCOMPLETE– HINT!)":" I wish I could hear her voice… then I’d know what to do. ",
-	"COMPLETION OF PUZZLE":" That’s what she looked like… we look happy. ",
-	"PHONE DIALOGUE 1": "You’ve reached: GLADWELL HOSPITAL. If you have a grievous injury, press 1. If you have medical queries, press 2. If you’d like to make an appointment, press 3. (That’s not it.) ",
-	"PHONE DIALOGUE 2":" BZZT. BZZT. LOW, GROWLING SOUNDS. ",
-	"PHONE DIALOGUE 3":" BZZT. BZZT. SOBBING, DISTANT. ",
-	"PHONE DIALOGUE 4":" (Man’s voice, angry.) Hello? Who is this? Hello? I know you’re there. I don’t ever want you calling this number again, you understand? ",
-	"PHONE DIALOGUE 5":" Breathing sounds. Phone hangs up. ",
-	"PHONE DIALOGUE 6 (CORRECT) ":" (Woman’s Voice) Hello? Hey, you. It’s good to hear your voice. I’ve got something very important to tell you. (pause) (laughter, clear and bright). Did you think it was really me? (Laughter). Thanks for calling. Leave a message or something. Bye!",
-	"CORRIDOR DIALOGUE (NECESSARY, TO SET SCENE FOR TIER 4)": "I don’t know if I want to, but I’m starting to remember. ",
-	"DIALOGUE 1 (TRIGGERED BY ENTERING CORRIDOR SECOND TIME)": "How many years did we spend here? How many times did we walk through these rooms? I cannot stop moving forward. I am a stone thrown, arrow fired, a promise set in some distant future.",
-	"DIALOGUE 2 (TRIGGERED BY ENTERING CORRIDOR THIRD TIME)":"Is it fair? Is there a world in which I hold her in my arms again, born anew, a slate wiped clean, to meet as if we’d never met, would we play out this same, tragic myth?",
-	"DIALOGUE 3 (TRIGGERED BY ENTERING CORRIDOR FOURTH TIME)": "There is no justice, not for something like me. There is no world in which I am allowed to hold. Unborn, filth, to never meet because we had to meet. I know how it ends. I know how it all ends.",
-	"UPON ENTERING BEDROOM FOR THE LAST TIME, CORRIDOR LOCKED": "There’s nothing left. There’s nowhere left to go. I know how it ends. I know how it all ends.",
-	"FINAL DIALOGUE (UPON TURNING BACK)": "I’m sorry. I’m sorry. I’m sorry. I’m sorry. I’m sorry. I’m sorry."
-}
-# end dialog variables
